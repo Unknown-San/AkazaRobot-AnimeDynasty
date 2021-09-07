a739b28d5359ce8ad67edd32a96e66c1e5c90101
UNKNOWN <84973544+Unknown-San@users.noreply.github.com>
2021-09-07 09:42:50 +0530

    Update chat_status.py

--- AkazaRobot/modules/helper_funcs/chat_status.py
+++ AkazaRobot/modules/helper_funcs/chat_status.py
@@ -402,6 +402,6 @@ def connection_status(func):
 
 
 # Workaround for circular import with connection.py
-from AsunaRobot.modules import connection
+from AkazaRobot.modules import connection
 
 connected = connection.connected
